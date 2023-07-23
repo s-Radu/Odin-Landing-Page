511306651babb2326781f153e543f441b8a8fab6
zmeu` <tsraduu@gmail.com>
2023-07-23 22:13:26 +0100

    Nav link on hamburger
    Fixed the nav links when hamburger menu is clicked, animation followes

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,7 @@
 const cards = document.querySelectorAll("[data-card]");
+const navLinks = document.getElementById("nav-links");
+const hamburgerLinks = document.querySelectorAll("[data-nav-link]");
+const hamburgerMenu = document.getElementById("hamburger-menu");
 
 // ? For each card that is hovered over, the class active is added and removed when hovered away.
 
@@ -13,4 +16,33 @@ cards.forEach((card) => {
     cardImg.classList.remove("active");
     cardText.classList.remove("active");
   });
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  if (!navLinks.classList.contains("hide")) {
+    navLinks.classList.add("hide");
+  } else {
+    navLinks.classList.remove("hide");
+  }
+  hamburgerLinks.forEach((link) => {
+    link.classList.toggle("active");
+  });
+  navLinks.classList.toggle("active");
+});
+
+function updateStae() {
+  if (window.innerWidth > 960) {
+    hamburgerMenu.classList.remove("active");
+    navLinks.classList.remove("hide");
+  } else {
+    hamburgerMenu.classList.add("active");
+    navLinks.classList.add("hide");
+  }
+}
+
+window.onload = () => {
+  updateStae();
+};
+window.addEventListener("resize", () => {
+  updateStae();
 });
